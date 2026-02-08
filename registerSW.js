964e2d8dd6c3e3e7a7b245d7946fdc558cb83747
tuhs1985 <77306309+tuhs1985@users.noreply.github.com>
2026-02-07 21:08:04 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pf2e-pathbuilder-tableplop/sw.js', { scope: '/pf2e-pathbuilder-tableplop/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
